d5196cc9e73a82cc84f19f00d678a523f21ba8ac
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2023-01-20 20:15:21 -0300

    NLW Setup Parte 19: ajuste nas cores, logo.svg e phosphor-react

--- nlw_front/src/App.tsx
+++ nlw_front/src/App.tsx
@@ -1,14 +1,16 @@
-import { Habit } from './components/Habit'
-import './App.css'
 import "./styles/global.css"
+import logo from "./assets/logo.svg"
 
 export function App() {
   return (
-    <div className="App">
-      <Habit completed={1}/>
-      <Habit completed={2}/>
-      <Habit completed={3}/>
-      <Habit completed={4}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logo} alt="Habits"/>
+          <button type="button"
+          className=" font-semibold rounded-lg px-6 py-4">Novo Hábito</button>
+        </div>
+      </div>
     </div>
   )
 }
